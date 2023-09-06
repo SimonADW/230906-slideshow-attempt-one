c5a668f284dbfcb185c3aacc9c1b8e1f73081648
SimonADW <simon-winter@outlook.com>
2023-09-06 14:37:47 +0200

    Add JS next image function

---
+++ main.js
@@ -0,0 +1,40 @@
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+
+let slideIndex = 0;
+showSlidesNext();
+
+nextButton.addEventListener("click", showSlidesNext);
+
+function showSlidesNext() {
+  let i;
+  let slides = document.getElementsByClassName("slideshow__image");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+
+
+
+
+// Function for previous button // 
+
+// previousButton.addEventListener("click", showSlidesPrevious);
+
+// function showSlidesPrevious() {
+//   let i;
+//   let slides = document.getElementsByClassName("slideshow__image");
+//   for (i = 0; i < slides.length; i--) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex < slides.length) {slideIndex = 1}
+//   slides[slideIndex+1].style.display = "block";
+// //   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
